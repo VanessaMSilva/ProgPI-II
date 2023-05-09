bdba854cfd994f7e61288780c5aa1314f765de69
Vanessa Machado Silva <lampetrolaboratorioufu@gmail.com>
2023-05-09 15:52:44 -0300

    cadastro
    cadastro

--- TrabalhoMuseu/cadastrar.php
+++ TrabalhoMuseu/cadastrar.php
@@ -3,27 +3,29 @@
     include("conexao.php");
 
     $nome = mysqli_real_escape_string($conexao, $_POST['nome']);
-    $usuario = mysqli_real_escape_string($conexao, $_POST['email']);
-    $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+    $usuario = mysqli_real_escape_string($conexao, $_POST['usuario1']);
+    $senha = mysqli_real_escape_string($conexao, $_POST['senha1']);
 
     $sql = "select count(*) as total from usuario where usuario = '$usuario'";
-    $result = mysqli_query($sql);
+    $sql = "select count(*) as total from usuario where usuario = '$usuario'";
+    $result = mysqli_query($conexao,$sql);
     $row = mysqli_fetch_assoc($result);
 
     if($row['total']==1){
         $_SESSION['usuario_existe'] = TRUE;
-        header('Location: cadastro.php ');
+        header('Location: index.php ');
+        echo "<script>alert('Erro cadastro')</script>";
         exit;
     }
 
-    $sql = "INSERT INTO usuario(nome, usuario,'$senha',NOW())";
+    $sql = "INSERT INTO usuario(nome, usuario,senha,data_cadastro) VALUES('$nome','$usuario',md5('{$senha}'),NOW())";
 
-    if($conexao->query($sql)==true){
+    if($conexao->query($sql)===true){
         $_SESSION['status_cadastro'] = true;
     }
 
     $conexao->close();
 
-    header('Location: cadastro.php');
+    header('Location: painel.php');
     exit;
 ?>
--- TrabalhoMuseu/index.php
+++ TrabalhoMuseu/index.php
@@ -11,32 +11,56 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema de Login - PHP + MySQL - Museu</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <header class="inicio">
-        <div class="Container Flex">
-            <div id="logo"><a href="index.html">Museu</a>
-            </div>
-            <nav>
-                <input type="checkbox" id="check">
-                <label id="label" for="check">&#x268c;</label>
-                <ul class="menu">
-                    <li><a href="login.php" data-target="#meuModal" data-toggle="modal">Login</a></li>
-                    <li><a href="atividades.html">Cadastro</a></li>
-                    <li><a href="contatos.html">Inicio</a></li>
-                    <li><a href="localizacao.html">Atividades</a></li>
 
-                </ul>
-            </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">
+            <img src="imagens/museu" width="30" height="30" class="d-inline-block align-top" alt="">
+            Museu
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <div id="direita" class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-success my-2 my-sm-0" id="login" data-target="#meuModal" data-toggle="modal" style="margin-right: 20px;" >Login</button>
+
+                <button class="btn btn-outline-success my-2 my-sm-0" id="cadastro" data-target="#meuModalCadastro" data-toggle="modal">Cadastro</button>
+            </div>
         </div>
-    </header>
-    <section class="hero is-success is-fullheight">
+    </nav>
+
+
+
+    <section class="corpo">
         <div class="modal fade" id=meuModal>
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -75,6 +99,7 @@ session_start();
                                             </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary">Login</button>
+
                                     </form>
                                 </div>
                             </div>
@@ -83,12 +108,52 @@ session_start();
                 </div>
             </div>
         </div>
-        <div><button data-target="#meuModal" data-toggle="modal">Ver login</button>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#modal-mensagem">
-                Exibir mensagem
-            </button>
-        </div>
+    </section>
 
+    <section class="corpo">
+        <div class="modal fade" id=meuModalCadastro>
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                        <h5 class="modal-title">Cadastro</h5>
+                        <button type="button" class="close" data-dismiss="modal"><span>×</span></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container has-text-centered">
+                            <div class="column is-4 is-offset-4">
+                                <div class="box">
+                                    <form action="cadastrar.php" method="POST">
+                                        <div class="field">
+                                            <div class="form-group">
+                                                <label for="exampleInputnome">Nome</label>
+                                                <input type="text" name="nome" class="form-control" id="exampleInputnome" aria-describedby="emailHelp" placeholder="Enter nome">
+                                            </div>
+                                        </div>
+                                        <div class="field">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail">Nome usuario</label>
+                                                <input type="text" name="usuario1" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter usuario">
+                                            </div>
+                                        </div>
+                                </div>
+                                <div class="field">
+                                    <div class="field">
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword">Senha</label>
+                                            <input type="password" name="senha1" class="form-control" id="exampleInputPassword" placeholder="Senha">
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </body>
 
